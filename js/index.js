6ccd30bd80944d40a307dbcf9f6fd61d25df9d6a
smokR47 <83354742+smokR47@users.noreply.github.com>
2021-11-21 16:16:11 +0100

    started generate function

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 function generate() {
-    
+    var exec = "echo Loading vic0s & pablit0s generated autoexec.cfg\n";
+    var from = document.forms[0];
+    console.log(form)
+    exec += "volume " + (from["master-vol-val"].value / 100).toString() + "\n" ;
+    exec += "snd_musicvolume_multiplier_inoverlay " + (from["music-in-steam-overlay-val"].value / 100).toString() + "\n";
+    exec += "voice_caster_scale " + (from["gotv-caster-vol-val"].value / 100).toString() + "\n";
+    console.log(exec)
 }
